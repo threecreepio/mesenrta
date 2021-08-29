e28882e546b54109904bf654f2e6b3f20044296d
threecreepio <me@creep.io>
2021-08-29 16:15:53 +0200

    add a mini frame counter to the input display

--- Core/VideoHud.cpp
+++ Core/VideoHud.cpp
@@ -11,6 +11,7 @@
 #include "Zapper.h"
 #include "MovieManager.h"
 #include "Console.h"
+void DrawSmallFramecounter(shared_ptr<Console> console, uint32_t* rgbaBuffer, FrameInfo& frameInfo, OverscanDimensions& overscan);
 
 void VideoHud::DrawHud(shared_ptr<Console> console, uint32_t *outputBuffer, FrameInfo frameInfo, OverscanDimensions overscan)
 {
@@ -26,6 +27,7 @@ void VideoHud::DrawHud(shared_ptr<Console> console, uint32_t *outputBuffer, Fram
 		}
 	}
 
+	DrawSmallFramecounter(console, outputBuffer, frameInfo, overscan);
 	DrawMovieIcons(console, outputBuffer, frameInfo, overscan);
 }
 
@@ -48,6 +50,139 @@ int GetSettingColor(shared_ptr<Console> console) {
 	if (cheats->HasCheats()) return 1;
 
 	return 0;
+}
+
+void BlendColors2(uint32_t* output, uint32_t input)
+{
+	uint8_t inA = (input >> 24) & 0xFF;
+	uint8_t inR = (input >> 16) & 0xFF;
+	uint8_t inG = (input >> 8) & 0xFF;
+	uint8_t inB = input & 0xFF;
+
+	uint8_t invertedAlpha = 255 - inA;
+	uint8_t outB = (uint8_t)((inA * inB + invertedAlpha * (*output & 0xFF)) >> 8);
+	uint8_t outG = (uint8_t)((inA * inG + invertedAlpha * ((*output >> 8) & 0xFF)) >> 8);
+	uint8_t outR = (uint8_t)((inA * inR + invertedAlpha * ((*output >> 16) & 0xFF)) >> 8);
+	*output = 0xFF000000 | (outR << 16) | (outG << 8) | outB;
+}
+
+
+const vector<uint32_t> _digits[0x10] = {
+	{  // 0
+		1, 1, 1, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+	},
+	{ // 1
+		0, 1, 1, 0, 0,
+		0, 0, 1, 0, 0,
+		0, 0, 1, 0, 0,
+		0, 0, 1, 0, 0,
+		0, 1, 1, 1, 0,
+	},
+	{ // 2
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+		1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0,
+	},
+	{ // 3
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+	},
+	{ // 4
+		1, 0, 0, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		0, 0, 0, 1, 0,
+	},
+	{ // 5
+		1, 1, 1, 1, 0,
+		1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+	},
+	{ // 6
+		1, 1, 1, 1, 0,
+		1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+	},
+	{ // 7
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		0, 0, 0, 1, 0,
+		0, 0, 0, 1, 0,
+		0, 0, 0, 1, 0,
+	},
+	{ // 8
+		1, 1, 1, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+	},
+	{ // 9
+		1, 1, 1, 1, 0,
+		1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0,
+		0, 0, 0, 1, 0,
+	},
+};
+
+void DrawSmallFramecounter(shared_ptr<Console> console, uint32_t* rgbaBuffer, FrameInfo& frameInfo, OverscanDimensions& overscan)
+{
+	bool axisInverted = (console->GetSettings()->GetScreenRotation() % 180) != 0;
+	InputDisplaySettings settings = console->GetSettings()->GetInputDisplaySettings();
+	uint32_t yStart, xStart;
+	switch (settings.DisplayPosition) {
+	case InputDisplayPosition::TopLeft:
+		xStart = 3;
+		yStart = 5;
+		break;
+	case InputDisplayPosition::TopRight:
+		xStart = frameInfo.Width - 40;
+		yStart = 5;
+		break;
+	case InputDisplayPosition::BottomLeft:
+		xStart = 3;
+		yStart = frameInfo.Height - 15;
+		break;
+	default:
+	case InputDisplayPosition::BottomRight:
+		xStart = frameInfo.Width - 40;
+		yStart = frameInfo.Height - 15;
+		break;
+	}
+
+	uint32_t settingColor = GetSettingColor(console);
+	int outlineColors[3] = { 0xEF111111, 0xEF117111, 0xEF711111 };
+	int number = console->GetFrameCount();
+	int start = 1;
+	for (int i = 1; i < 6; i += 1) {
+		int digit = (number / start) % 10;
+		int digitX = 39 + (i * -5);
+		for (int x = 0; x < 5; ++x) {
+			for (int y = 0; y < 5; ++y) {
+				uint32_t bufferPos = ((yStart + y) * frameInfo.Width) + (xStart + x) + digitX;
+				int bitset = _digits[digit][y * 5 + x];
+				if (bitset > 0) {
+					BlendColors2(rgbaBuffer + bufferPos, outlineColors[settingColor]);
+				}
+			}
+		}
+		start *= 10;
+	}
 }
 
 bool VideoHud::DisplayControllerInput(shared_ptr<Console> console, ControlDeviceState &state, int inputPort, uint32_t *outputBuffer, FrameInfo &frameInfo, OverscanDimensions &overscan, uint32_t displayIndex)
@@ -284,13 +419,13 @@ const vector<uint32_t> VideoHud::_recordIcon = {
 };
 
 const vector<uint32_t> VideoHud::_gamePads[4] = {
- { 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
-	9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
-	9,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
-	9,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,9,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
-	9,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
-	9,0,7,7,7,9,9,9,8,8,8,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
-	9,0,7,7,7,9,9,9,8,8,8,0,0,0,0,0,0,9,9,9,0,0,0,0,0,0,0,2,2,0,0,0,0,1,1,0,0,9,
+ { 9,9,9,9,9,9,9,9,9,9,9,9,9,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	9,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	9,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	9,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	9,0,7,7,7,9,9,9,8,8,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
+	9,0,7,7,7,9,9,9,8,8,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0,1,1,0,0,9,
 	9,0,7,7,7,9,9,9,8,8,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,1,1,1,1,0,9,
 	9,0,0,0,0,6,6,6,0,0,0,0,0,0,3,3,3,3,0,4,4,4,4,0,0,0,2,2,2,2,0,0,1,1,1,1,0,9,
 	9,0,0,0,0,6,6,6,0,0,0,0,0,0,3,3,3,3,0,4,4,4,4,0,0,0,0,2,2,0,0,0,0,1,1,0,0,9,
